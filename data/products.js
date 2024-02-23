b6318094df2771e88c4d90cdbcbfab8c5ce744ca
alpha02 <you@example.com>
2024-02-23 17:08:30 +0700

    use products.js file to store all data

---
+++ data/products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    image: "image/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+  },
+  {
+    image: "/image/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127,
+    },
+    priceCents: 2095,
+  },
+  {
+    image: "/image/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 57,
+    },
+    priceCents: 799,
+  },
+];
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    image: "image/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "/image/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "/image/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 57,
-    },
-    priceCents: 799,
-  },
-];
-
 let productHTML = "";
 
 products.forEach((product) => {
@@ -65,4 +35,4 @@ products.forEach((product) => {
   `;
 });
 
-document.querySelector(".product-container").innerHTML = productHTML;
+document.querySelector(".js-products-container").innerHTML = productHTML;
